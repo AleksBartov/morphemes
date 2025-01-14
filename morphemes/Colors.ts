4e1d3f48812099b7ad202686b5943e920c92262a
AleksBartov <37273772+AleksBartov@users.noreply.github.com>
2025-01-14 15:36:20 +0300

    just start to work. there is need to think about app's arcitecture

---
+++ morphemes/Colors.ts
@@ -0,0 +1,6 @@
+export const AppColors = {
+  blue: "#233040",
+  lightBlue: "#2c6485",
+  charcoal: "#394956",
+  platinum: "#e8e8e8",
+};
--- morphemes/app/index.tsx
+++ morphemes/app/index.tsx
@@ -1,15 +1,137 @@
-import { Text, View } from "react-native";
+import { AppColors } from "@/Colors";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+const { width, height } = Dimensions.get("window");
+const SING_BOX_SIZE = width * 0.2;
+const MAX_BOTTOM = height / 2 - SING_BOX_SIZE;
+const START_POS = width / 2 - SING_BOX_SIZE / 2;
 
 export default function Index() {
+  const translateX = useSharedValue(START_POS);
+  const translateY = useSharedValue(0);
+  const offsetX = useSharedValue(0);
+  const offsetY = useSharedValue(0);
+  const SING_BOX_WIDTH = useSharedValue(SING_BOX_SIZE);
+  const offsetPinch = useSharedValue(0);
+
+  const pan = Gesture.Pan()
+    .onBegin(() => {
+      offsetX.value = translateX.value;
+      offsetY.value = translateY.value;
+    })
+    .onChange(({ translationX, translationY }) => {
+      translateX.value = translationX + offsetX.value;
+      if (translationY + offsetY.value > MAX_BOTTOM) {
+        translateY.value = MAX_BOTTOM;
+      } else {
+        translateY.value = translationY + offsetY.value;
+      }
+    })
+    .onEnd(({ translationY }) => {
+      if (translationY + offsetY.value < MAX_BOTTOM / 2) {
+        translateY.value = withTiming(0);
+      } else {
+        translateY.value = withTiming(MAX_BOTTOM);
+      }
+      translateX.value = withTiming(START_POS);
+    });
+  const pinch = Gesture.Pinch()
+    .onBegin(() => {
+      offsetPinch.value = SING_BOX_WIDTH.value;
+    })
+    .onUpdate(({ scale }) => {
+      SING_BOX_WIDTH.value = offsetPinch.value * scale;
+    })
+    .onEnd(() => {});
+  const panAndPinch = Gesture.Race(pan, pinch);
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      width: SING_BOX_WIDTH.value,
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  }, []);
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: AppColors.platinum,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <GestureDetector gesture={panAndPinch}>
+        <Animated.View style={[styles.sing, rStyle]}>
+          <Text style={styles.sing_text}>^</Text>
+        </Animated.View>
+      </GestureDetector>
+      <View style={styles.box_for_letter}></View>
+      <View style={styles.box_for_letter_1}></View>
+      <View style={styles.box_for_letter_2}></View>
+      <View style={styles.box_for_letter_3}></View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sing: {
+    position: "absolute",
+    height: SING_BOX_SIZE * 2,
+    borderColor: AppColors.charcoal,
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingVertical: 24,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  box_for_letter: {
+    position: "absolute",
+    top: MAX_BOTTOM + SING_BOX_SIZE,
+    right: START_POS - SING_BOX_SIZE * 2,
+    width: SING_BOX_SIZE,
+    height: SING_BOX_SIZE,
+    borderColor: AppColors.lightBlue,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  box_for_letter_1: {
+    position: "absolute",
+    top: MAX_BOTTOM + SING_BOX_SIZE,
+    right: START_POS - SING_BOX_SIZE,
+    width: SING_BOX_SIZE,
+    height: SING_BOX_SIZE,
+    borderColor: AppColors.lightBlue,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  box_for_letter_2: {
+    position: "absolute",
+    top: MAX_BOTTOM + SING_BOX_SIZE,
+    right: START_POS,
+    width: SING_BOX_SIZE,
+    height: SING_BOX_SIZE,
+    borderColor: AppColors.lightBlue,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  box_for_letter_3: {
+    position: "absolute",
+    top: MAX_BOTTOM + SING_BOX_SIZE,
+    right: START_POS + SING_BOX_SIZE,
+    width: SING_BOX_SIZE,
+    height: SING_BOX_SIZE,
+    borderColor: AppColors.lightBlue,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  sing_text: {
+    fontSize: 65,
+  },
+});
